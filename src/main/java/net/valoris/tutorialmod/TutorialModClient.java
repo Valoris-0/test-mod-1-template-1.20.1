de707c33f8a69353903cd2c5fe26ed8f5bc7e286
Valoris-0 <gamerabkboy7@gmail.com>
2024-12-26 17:08:12 +0100

    block of ruby added

---
+++ src/main/java/net/valoris/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.valoris.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/valoris/tutorialmod/item/ModItemGroups.java
@@ -0,0 +1,34 @@
+package net.valoris.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.valoris.tutorialmod.TutorialMod;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.valoris.tutorialmod.TutorialMod;
+import net.valoris.tutorialmod.block.ModBlocks;
+
+public class ModItemGroups {
+    public static final ItemGroup RUBY_GROUP = Registry.register(Registries.ITEM_GROUP,
+            new Identifier(TutorialMod.MOD_ID, "ruby"),
+            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.ruby"))
+                    .icon(() -> new ItemStack(ModItems.RUBY)).entries((displayContext, entries) -> {
+                        entries.add(ModItems.RUBY);
+                        entries.add(ModItems.RAW_RUBY);
+                        entries.add(ModItems.RAW_DIAMOND);
+
+                        entries.add(Items.DIAMOND);
+
+                        entries.add(ModBlocks.RUBY_BLOCK);
+
+                    }).build());
+
+
+    public static void registerItemGroups() {
+        TutorialMod.LOGGER.info("Registering Item Groups for " + TutorialMod.MOD_ID);
+    }
+}
